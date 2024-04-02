aa732098e44582b909b571bb157076ba53adf604
Omagar1 <josiah.rowden@gmail.com>
2024-04-02 10:43:47 +0100

    Added foreign Keys

--- DBStructure.sql
+++ DBStructure.sql
@@ -1,31 +1,5 @@
-CREATE TABLE taskList(
-    ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL, 
-    deadline DATETIME,
-    collab BOOLEAN NOT NULL DEFAULT false,
-    `Priority` INT NOT NULL DEFAULT 2,
-    ownerID INT NOT NULL
-); 
 
-CREATE TABLE task(
-    ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL, 
-    deadline DATETIME,
-    collab BOOLEAN NOT NULL DEFAULT false,
-    `Priority` INT NOT NULL DEFAULT 2,
-    taskListID INT NOT NULL
-);
-
-CREATE TABLE stage(
-    ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL, 
-    weighting  DECIMAL(5,2),
-    complete BOOLEAN NOT NULL DEFAULT false,
-    dateTimeCompleted DATETIME,
-    completedBy INT,
-    taskID INT NOT NULL
-); 
-
+DROP  TABLE IF EXISTS user;
 CREATE TABLE user(
     ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
@@ -33,19 +7,57 @@ CREATE TABLE user(
     email VARCHAR(255) NOT NULL,
     collabCode VARCHAR(255)
 );
-CREATE TABLE role(
+
+DROP  TABLE IF EXISTS taskList;
+CREATE TABLE taskList(
     ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL, 
-    `createPermission` BOOLEAN NOT NULL DEFAULT false,
-    `editPermission` BOOLEAN NOT NULL DEFAULT false,
-    `deletePermission` BOOLEAN NOT NULL DEFAULT false
+    deadline DATETIME,
+    collab BOOLEAN NOT NULL DEFAULT false,
+    `Priority` INT NOT NULL DEFAULT 2,
+    ownerID INT NOT NULL,
+    FOREIGN KEY(ownerID) REFERENCES user(ID)
 );
 
+DROP  TABLE IF EXISTS task;
+CREATE TABLE task(
+    ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL, 
+    deadline DATETIME,
+    collab BOOLEAN NOT NULL DEFAULT false,
+    `Priority` INT NOT NULL DEFAULT 2,
+    taskListID INT NOT NULL,
+    FOREIGN KEY (taskListID) REFERENCES taskList(ID)
+);
 
+DROP  TABLE IF EXISTS stage;
+CREATE TABLE stage(
+    ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL, 
+    weighting  DECIMAL(5,2),
+    complete BOOLEAN NOT NULL DEFAULT false,
+    dateTimeCompleted DATETIME,
+    completedBy INT,
+    taskID INT NOT NULL,
+    FOREIGN KEY (taskID) REFERENCES task(ID)
+); 
+
+
+-- CREATE TABLE role(
+--     ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     `name` VARCHAR(255) NOT NULL, 
+--     `createPermission` BOOLEAN NOT NULL DEFAULT false,
+--     `editPermission` BOOLEAN NOT NULL DEFAULT false,
+--     `deletePermission` BOOLEAN NOT NULL DEFAULT false
+-- );
+
+DROP  TABLE IF EXISTS taskListCollab;
 CREATE TABLE taskListCollab(
     ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     taskListID INT NOT NULL,
-    userID INT NOT NULL
+    userID INT NOT NULL,
+    CONSTRAINT taskListID_fk FOREIGN KEY (taskListID) REFERENCES taskList(ID),
+    CONSTRAINT userID_fk FOREIGN KEY (userID) REFERENCES user(ID)
     -- roleID INT NOT NULL
 );
 
